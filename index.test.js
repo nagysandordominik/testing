012a75b920675a1fa09058f6766d399c3080fb7a
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-03-01 19:24:13 +0100

    change: wrap if statement with node's assert module

--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 const { forEach, map } = require('./index');
-
+const assert = require('assert');
 const test = (desc, fn) => {
     console.log('----', desc);
     try {
@@ -15,25 +15,18 @@ test('The forEach function', () => {
         sum += value;
     });
     
-    if (sum !== 6) {
-        throw new Error('Expected summing array to equal 6!')
-    } 
+    assert.strictEqual(sum, 6 , 'Expected forEach to sum the array')
+    
 });
 
 test('The map function', () => {
     const result = map([1,2,3], value => {
         return value * 2;
     });
-    
-    if (result[0] !== 2 ) {
-        throw new Error (`Expected to find 2, but found ${result[0]}`)
-    }
-    if (result[1] !== 4 ) {
-        throw new Error (`Expected to find 4, but found ${result[1]}`)
-    }
-    if (result[2] !== 6 ) {
-        throw new Error (`Expected to find 6, but found ${result[2]}`)
-    }
+    // assert.strictEqual(result[0], 2)
+    // assert.strictEqual(result[1], 4)
+    // assert.strictEqual(result[2], 7)
+    assert.deepStrictEqual(result, [2,4,6]);
 });
 
 
